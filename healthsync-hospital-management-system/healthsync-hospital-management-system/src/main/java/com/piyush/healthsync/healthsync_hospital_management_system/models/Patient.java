3ec93d90d5df4b62e04796b2c9c0483f42b31025
PiyushGhadle <piyushghadle123@mail.com>
2026-06-26 23:23:35 +0530

    Implement Patient Management

---
+++ healthsync-hospital-management-system/healthsync-hospital-management-system/src/main/java/com/piyush/healthsync/healthsync_hospital_management_system/models/Patient.java
@@ -0,0 +1,25 @@
+package com.piyush.healthsync.healthsync_hospital_management_system.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String gender;
+    private int age;
+
+}
